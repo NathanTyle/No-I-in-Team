4c955ef5e0b78757b33d4be3253d96e96d1ab30d
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-10-27 11:02:06 -0500

    added Manager function to Index.js

--- index.js
+++ index.js
@@ -16,3 +16,63 @@ const outputPath = path.join(DIST_DIR, 'index.html');
 
 const teamArr = [];
 const idArr = [];
+
+function initApp() {
+
+    function addManager() {
+        console.log();
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is your manager's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Manager's name is not valid, try again.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is your manager's ID?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "ID is not valid, try again.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is your manager's email?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Email address can not be empty.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is your manager's office number? (format: '555' - '555' - '5555')",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[0-9]\d*$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Your Office number must be valid, try again.";
+                }
+            }
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamArr.push(manager);
+            idArr.push(answers.managerId);
+            addTeam();
+        });
+    };
